484b437bdcb9774c9634ed43c1cc310f336e7ed7
Arfi <arfihussain27@gmail.com>
2024-04-30 15:53:29 +0530

    final commit

--- script.js
+++ script.js
@@ -14,14 +14,16 @@ buttons.forEach(function(btn) {
         console.log(text);
 
         // Clear
-        if (text === 'C') {
+        if (text === 'AC') {
             display.textContent = '';
             result = 0;
             isDecimal = false;
             operands = [];
         }
+
         // Operations
-        else if (text === 'x' || text == '+' || text == '-') {
+        else if (text === 'x' || text === '+' || text === '-' || text === '/' || text === '%') {
+            result = Number(result);
             isDecimal = false;
             if (result !== 0) {
                 operands.push(result);
@@ -31,22 +33,26 @@ buttons.forEach(function(btn) {
             // Remove previous button highlight
             const currentActiveBtn = document.querySelector(".active");
             if (currentActiveBtn) {
-                currentActiveBtn.style.backgroundColor = "rgb(133, 143, 158)";
+                currentActiveBtn.style.backgroundColor = "rgb(122, 153, 172)";
                 currentActiveBtn.classList.remove("active");
             }
 
             // Add button highlight
             e.target.classList.add("active");
-            document.querySelector('.active').style.backgroundColor = "rgb(200, 200, 200)";
+            document.querySelector('.active').style.backgroundColor = "rgb(166, 185, 204)";
             
             // If there are already 2 operands, then calculate
             if (operands.length >= 2) {
                 if (text == '+')
-                    result = operands[0] + operands[1]
+                    result = operands[0] + operands[1];
                 else if (text == '-')
-                    result = operands[0] - operands[1]
+                    result = operands[0] - operands[1];
                 else if (text == 'x')
-                    result = operands[0] * operands[1]
+                    result = operands[0] * operands[1];
+                else if (text == '/') 
+                    result = operands[0] / operands[1];
+                else if (text == '%')
+                    result = operands[0] % operands[1];
                 
                 display.textContent = result;
                 operands = [];
@@ -56,10 +62,12 @@ buttons.forEach(function(btn) {
         
         // =
         else if (text === '=') {
+            result = Number(result);
+
             // Remove button highlight
             const currentActiveBtn = document.querySelector(".active");
             if (currentActiveBtn) {
-                currentActiveBtn.style.backgroundColor = "rgb(133, 143, 158)";
+                currentActiveBtn.style.backgroundColor = "rgb(122, 153, 172)";
                 currentActiveBtn.classList.remove("active");
             }
             if (operands.length == 0) {
@@ -78,13 +86,19 @@ buttons.forEach(function(btn) {
                 else if (currentActiveBtn.textContent === 'x') {
                     result = operands.reduce((accu, val) => accu *= val);
                 }
+                else if (currentActiveBtn.textContent === '/') {
+                    result = operands.reduce((accu, val) => accu /= val);
+                }
+                else if (currentActiveBtn.textContent === '%') {
+                    result = operands.reduce((accu, val) => accu %= val);
+                }
                 
                 display.textContent = result;
                 console.log(operands);
                 operands.length = 0;
             }
             console.log(result);
-            console.log(currentActiveBtn.textContent);
+            // console.log(currentActiveBtn.textContent);
         }
         // .
         else if (text === '.') {
@@ -95,21 +109,48 @@ buttons.forEach(function(btn) {
                 prevText = text;
             }
         }
+
+        // Undo
+        else if (text == 'C') {
+            let currText = display.textContent;
+            if (currText.at(-1) == ".") {
+                console.log("last is .");
+                prevText = '';
+                isDecimal = false;
+            }
+            currText = currText.slice(0, currText.length - 1);
+            result = Number(currText);
+            console.log(result);
+            display.textContent = currText;
+        }
+
+        // 00
+        else if (text == '00') {
+            if (prevText == '.') {
+                result = result+'.'+text;
+                prevText = '';
+            }
+            else {
+                result = result + text;
+            }
+            display.textContent = `${result}`;
+        }
+
         // Numbers
         else if (Number(text) >= 0 && Number((text) <= 9)) {
             if(isDecimal) {
                 if (prevText === '.') {
-                    result = Number(`${result}.${text}`);
+                    result = `${result}.${text}`;
                     prevText = '';
                 }
                 else {
                     text = `${result}` + text;
-                    result = Number(`${text}`);
+                    result = text;
                     console.log(result);
                 }
             }
             else if (!isDecimal) {
-                result = Number(text);
+                result = Number(result+text);
             }
             display.textContent = `${result}`;
         }
